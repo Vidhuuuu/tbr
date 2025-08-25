4122937f32f4758045c8337049c0b66e1c3673b0
vidhu <vidhudash@gmail.com>
2025-08-25 22:06:51 +0530

    feat: flags for title and author

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+type TbrItem struct {
+	ID     int
+	Title  string
+	Author string
+	Date   time.Time
+}
+
 func main() {
-	println("start")
+	title := flag.String("title", "empty-title", "Title of the book")
+	author := flag.String("author", "anonymous", "Author of the book")
+	flag.Parse()
+
+	if flag.Parsed() {
+		fmt.Printf("title = %s\nauthor = %s\n", *title, *author)
+	}
 }
